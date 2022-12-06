9c71cd8e9bfff63aa90cb3f0986fc05945eaf659
parham <Parham_minoo@yahoo.com>
2022-12-06 06:31:22 -0800

    creating home controller

---
+++ src/controllers/site/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers\Site;
+
+use App\Classes\Gate;
+use App\Classes\Post;
+use App\Classes\User;
+use App\Controllers\Refrence\SiteRefrenceController;
+use App\Models\PostModel;
+
+class HomeController extends SiteRefrenceController {
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function home()
+    {
+        $user = new User();
+        $check = $user->isLogin();
+        if ($check['login']) {
+            $currentUser = $check['user'];
+            $user->setName($currentUser['name']);
+            $user->setEmail($currentUser['email']);
+            $user->setAdmin($currentUser['admin']);
+            $user->setId($currentUser['id']);
+            
+            $postModel = new PostModel();
+            $result = $postModel->getById(1);
+            $post = new Post();
+            $post->setTitle($result[0]['title']);
+            $post->setDescription($result[0]['description']);
+            $post->setUser($result[0]['user']);
+
+            // print_f(Gate::allows('post_create', $user));
+            print_f(Gate::allows('post_update', $user, $post));
+        } else {
+            //redirect to register page
+            // Tools::redirect(BASE_URI . "/register/");
+        }
+    }
+}
